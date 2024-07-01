bf01d3b2b8ca6ab45bc27ab4ebf13a49f5539ba2
Lucas Silva <L.Silva.E@hotmail.com>
2024-06-30 22:03:07 -0400

    ✨ Added 'environment' file and 'env' example file

---
+++ src/constants/environment.ts
@@ -0,0 +1,4 @@
+export const API_URL = `${import.meta.env.VITE_API_URL}`;
+export const API_VERSION = `${import.meta.env.VITE_API_VERSION}`;
+export const API_BASE_URL = `${API_URL}/${API_VERSION}`;
+export const API_KEY = `${import.meta.env.VITE_API_KEY}`;
