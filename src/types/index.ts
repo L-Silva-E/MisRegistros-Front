7ab5bab8954143e5f59da7b69c66ec24595ef3b2
Lucas Silva <L.Silva.E@hotmail.com>
2024-06-30 22:03:22 -0400

    ✨ Added index with types

---
+++ src/types/index.ts
@@ -0,0 +1,43 @@
+export type SearchForm = {
+  search: string;
+};
+
+export type Category = {
+  id?: number;
+  name: string;
+  created_at?: string;
+  updated_at?: string;
+};
+
+export type Ingredient = {
+  quantity: number;
+  ingredient: {
+    name: string;
+    unit: string;
+  };
+};
+
+export type Step = {
+  number: number;
+  instruction: string;
+};
+
+export type Recipe = {
+  id?: number;
+  idCategory: number;
+  idOrigin: number;
+  name: string;
+  description: string;
+  score: number;
+  thumbnail: string;
+  created_at?: string;
+  updated_at?: string;
+  category?: Category;
+  origin?: Category;
+  ingredients: Ingredient[];
+  steps: Step[];
+};
+
+export type RecipeDetail = {
+  [key: string]: string;
+};
