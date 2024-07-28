8be987b51c3e550bfe4832319fdfd6dde226a7a6
Lucas Silva <L.Silva.E@hotmail.com>
2024-07-27 21:17:28 -0400

    ✨ Added modal to create 'Recipes'

--- src/components/RecipeModalCreate.tsx
+++ src/components/RecipeModalCreate.tsx
@@ -2,8 +2,8 @@ import React, { useState } from "react";
 import {
   Button,
   Center,
+  Flex,
   FormControl,
-  FormHelperText,
   FormLabel,
   HStack,
   Input,
@@ -11,18 +11,18 @@ import {
   ModalBody,
   ModalCloseButton,
   ModalContent,
-  ModalFooter,
   ModalHeader,
   ModalOverlay,
   Select,
   Textarea,
-  VStack,
   useColorModeValue,
 } from "@chakra-ui/react";
 import getDataAxios from "../hooks/getDataAxios";
+import usePostDataAxios from "../hooks/postDataAxios";
 import { API_BASE_URL } from "../constants/environment";
 import { Category, IngredientDetail, Origin } from "../types";
 import { FaPlus, FaTrash } from "react-icons/fa";
+import { useForm } from "react-hook-form";
 
 type Props = {
   isOpen: boolean;
@@ -31,19 +31,19 @@ type Props = {
 
 const defaultCategory = { id: 0, name: "" };
 const defaultOrigin = { id: 0, name: "" };
-const defaultIngredientState = { id: 0, amount: "" };
+const defaultIngredientState = { id: "0", quantity: "" };
 
 function RecipeModalCreate({ isOpen, onClose }: Props) {
   const initialRef = React.useRef(null);
   const finalRef = React.useRef(null);
 
-  // ~ States: Category, Origin, Ingredient
+  const { register, handleSubmit } = useForm();
+
   const [selectedCategory, setSelectedCategory] =
     useState<Category>(defaultCategory);
   const [selectedOrigin, setSelectedOrigin] = useState<Origin>(defaultOrigin);
   const [ingredients, setIngredients] = useState([defaultIngredientState]);
 
-  // ~ GetDataAxios: Category, Origin, Ingredient
   const { loading: loadingCategories, data: dataCategories } =
     getDataAxios<Category>(`${API_BASE_URL}/category?page=0&limit=100`);
   const { loading: loadingOrigins, data: dataOrigins } = getDataAxios<Origin>(
@@ -65,7 +65,7 @@ function RecipeModalCreate({ isOpen, onClose }: Props) {
   ) => {
     const newIngredients = ingredients.map((ingredient, i) =>
       i === index
-        ? { ...ingredient, [field]: value === "" ? 0 : value }
+        ? { ...ingredient, [field]: value === "" ? "0" : value }
         : ingredient
     );
     setIngredients(newIngredients);
@@ -74,6 +74,39 @@ function RecipeModalCreate({ isOpen, onClose }: Props) {
   const removeIngredientRow = (index: number) => {
     const newIngredients = ingredients.filter((_, i) => i !== index);
     setIngredients(newIngredients);
+  };
+
+  const { postData } = usePostDataAxios<any>();
+
+  const onSubmit = async (dataForm: any) => {
+    const bodyRecipe = {
+      idCategory: selectedCategory.id,
+      idOrigin: selectedOrigin.id,
+      name: dataForm.name,
+      description: dataForm.description,
+      score: parseInt(dataForm.score),
+      thumbnail: dataForm.thumbnail
+        ? dataForm.thumbnail
+        : "https://placehold.co/800x600/1C4532/C6F6D5",
+      ingredients: ingredients.map((ingredient) => {
+        return {
+          id: parseInt(ingredient.id),
+          quantity: parseInt(ingredient.quantity),
+        };
+      }),
+      steps: dataForm.steps.split("\n").map((step: string, index: number) => {
+        return {
+          number: index + 1,
+          instruction: step,
+        };
+      }),
+    };
+
+    try {
+      await postData(`${API_BASE_URL}/recipe`, bodyRecipe);
+    } catch (error) {
+      console.error("Error creating recipe:", error);
+    }
   };
 
   return (
@@ -87,34 +120,58 @@ function RecipeModalCreate({ isOpen, onClose }: Props) {
       >
         <ModalOverlay />
         <ModalContent>
-          <ModalHeader>Crear Receta</ModalHeader>
-          <ModalCloseButton />
+          <ModalHeader>
+            Crear Receta
+            <ModalCloseButton />
+          </ModalHeader>
           <ModalBody pb={6}>
-            <FormControl>
-              <FormLabel>Nombre</FormLabel>
-              <Input ref={initialRef} placeholder="" />
-            </FormControl>
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <FormControl isRequired>
+                <FormLabel>Nombre</FormLabel>
+                <Input {...register("name")} autoComplete="off" />
+              </FormControl>
 
-            <FormControl mt={4}>
-              <HStack>
+              <FormControl mt={4}>
                 <FormLabel>Descripción</FormLabel>
-                <FormHelperText
-                  ml={-4}
-                  mb={1}
-                  color="gray.500"
-                  fontStyle="italic"
-                >
-                  *Opcional
-                </FormHelperText>
-              </HStack>
-              <Textarea placeholder="" size="sm" resize="none" />
-            </FormControl>
+                <Textarea
+                  size="sm"
+                  resize="none"
+                  {...register("description")}
+                />
+              </FormControl>
 
-            <HStack mt={4}>
-              <VStack flex={1}>
-                <FormControl>
+              <FormControl mt={4}>
+                <FormLabel>Imagen</FormLabel>
+                <Input
+                  placeholder="Link de la imagen"
+                  {...register("thumbnail")}
+                />
+              </FormControl>
+
+              <Flex mt={4}>
+                <FormControl isRequired width="60%">
+                  <FormLabel>Puntuación</FormLabel>
+                  <Select placeholder="-" {...register("score")}>
+                    <option value="5">5 - ★★★★★</option>
+                    <option value="4">4 - ★★★★</option>
+                    <option value="3">3 - ★★★</option>
+                    <option value="2">2 - ★★</option>
+                    <option value="1">1 - ★</option>
+                  </Select>
+                </FormControl>
+
+                <FormControl isRequired mx={2}>
                   <FormLabel>Categoría</FormLabel>
-                  <Select width="100%" placeholder="-">
+                  <Select
+                    placeholder="-"
+                    onChange={(e) =>
+                      setSelectedCategory(
+                        dataCategories.find(
+                          (category) => category.name === e.target.value
+                        ) || defaultCategory
+                      )
+                    }
+                  >
                     {loadingCategories && (
                       <option value="emptyCategory">Cargando...</option>
                     )}
@@ -126,11 +183,19 @@ function RecipeModalCreate({ isOpen, onClose }: Props) {
                       ))}
                   </Select>
                 </FormControl>
-              </VStack>
-              <VStack flex={1}>
-                <FormControl>
+
+                <FormControl isRequired>
                   <FormLabel>Origen</FormLabel>
-                  <Select width="100%" placeholder="-">
+                  <Select
+                    placeholder="-"
+                    onChange={(e) =>
+                      setSelectedOrigin(
+                        dataOrigins.find(
+                          (origin) => origin.name === e.target.value
+                        ) || defaultOrigin
+                      )
+                    }
+                  >
                     {loadingOrigins && (
                       <option value="emptyOrigin">Cargando...</option>
                     )}
@@ -142,87 +207,96 @@ function RecipeModalCreate({ isOpen, onClose }: Props) {
                       ))}
                   </Select>
                 </FormControl>
-              </VStack>
-            </HStack>
+              </Flex>
 
-            <FormControl mt={4}>
-              <FormLabel>Ingredientes</FormLabel>
-              {ingredients.map((ingredient, index) => (
-                <HStack key={index} mt={2}>
-                  <Select
-                    placeholder="-"
-                    value={ingredient.id}
-                    onChange={(e) =>
-                      handleIngredientChange(index, "id", e.target.value)
-                    }
-                  >
-                    {loadingIngredients && (
-                      <option value="emptyIngredient">Cargando...</option>
-                    )}
-                    {!loadingIngredients &&
-                      dataIngredients.map((ingredient) => (
-                        <option key={ingredient.id} value={ingredient.id}>
-                          {ingredient.name}
-                        </option>
-                      ))}
-                  </Select>
-                  <Input
-                    width="30%"
-                    placeholder="Cantidad"
-                    value={ingredient.amount}
-                    inputMode="numeric"
-                    onChange={(e) =>
-                      handleIngredientChange(index, "amount", e.target.value)
-                    }
-                  />
-                  <Center
-                    h={10}
-                    width="15%"
-                    color={useColorModeValue("gray.800", "white")}
-                    backgroundColor={useColorModeValue("gray.300", "gray.600")}
-                    borderRadius={5}
-                  >
-                    {ingredients[index].id === 0
-                      ? "-"
-                      : dataIngredients[ingredients[index].id - 1].unit}
-                  </Center>
-                  <Button
-                    variant="deleteButton"
-                    h={10}
-                    width="10%"
-                    onClick={() => removeIngredientRow(index)}
-                  >
-                    <FaTrash color={useColorModeValue("#1A202C", "white")} />
-                  </Button>
-                </HStack>
-              ))}
-            </FormControl>
-            <Button
-              h={8}
-              width="100%"
-              mt={2}
-              variant="addRowButton"
-              onClick={addIngredientRow}
-            >
-              <FaPlus color={useColorModeValue("#1A202C", "white")} />
-            </Button>
-
-            <FormControl mt={4}>
-              <FormLabel>Pasos</FormLabel>
-              <Textarea
-                size="sm"
-                resize="none"
-                placeholder="Separar cada paso con un 'Enter' o 'Salto de línea'"
-              />
-            </FormControl>
+              <FormControl isRequired mt={4}>
+                <FormLabel>Ingredientes</FormLabel>
+                {ingredients.map((ingredient, index) => (
+                  <HStack key={index} mt={2}>
+                    <Select
+                      placeholder="-"
+                      value={ingredient.id}
+                      onChange={(e) =>
+                        handleIngredientChange(index, "id", e.target.value)
+                      }
+                    >
+                      {loadingIngredients && (
+                        <option value="emptyIngredient">Cargando...</option>
+                      )}
+                      {!loadingIngredients &&
+                        dataIngredients.map((ingredient) => (
+                          <option key={ingredient.id} value={ingredient.id}>
+                            {ingredient.name}
+                          </option>
+                        ))}
+                    </Select>
+                    <Input
+                      width="30%"
+                      placeholder="Cantidad"
+                      value={ingredient.quantity}
+                      autoComplete="off"
+                      onChange={(e) =>
+                        handleIngredientChange(
+                          index,
+                          "quantity",
+                          e.target.value
+                        )
+                      }
+                    />
+                    <Center
+                      h={10}
+                      width="15%"
+                      color={useColorModeValue("gray.800", "white")}
+                      backgroundColor={useColorModeValue(
+                        "gray.300",
+                        "gray.600"
+                      )}
+                      borderRadius={5}
+                    >
+                      {ingredients[index].id === "0"
+                        ? "-"
+                        : dataIngredients.find(
+                            (item) =>
+                              item.id === parseInt(ingredients[index].id)
+                          )?.unit}
+                    </Center>
+                    <Button
+                      variant="deleteButton"
+                      h={10}
+                      width="10%"
+                      onClick={() => removeIngredientRow(index)}
+                    >
+                      <FaTrash color={useColorModeValue("#1A202C", "white")} />
+                    </Button>
+                  </HStack>
+                ))}
+              </FormControl>
+              <Button
+                h={8}
+                width="100%"
+                mt={2}
+                variant="addRowButton"
+                onClick={addIngredientRow}
+              >
+                <FaPlus color={useColorModeValue("#1A202C", "white")} />
+              </Button>
+              <FormControl isRequired mt={4}>
+                <FormLabel>Pasos</FormLabel>
+                <Textarea
+                  size="sm"
+                  resize="none"
+                  placeholder="Separar cada paso con un 'Enter' o 'Salto de línea'"
+                  {...register("steps")}
+                />
+              </FormControl>
+              <Button mt={4} type="submit" variant="greenButton">
+                Guardar
+              </Button>
+              <Button mt={4} ml={4} onClick={onClose}>
+                Cancelar
+              </Button>
+            </form>
           </ModalBody>
-
-          <ModalFooter>
-            <Button mr={3} variant="greenButton">
-              Guardar
-            </Button>
-            <Button onClick={onClose}>Cancelar</Button>
-          </ModalFooter>
         </ModalContent>
       </Modal>
     </>
