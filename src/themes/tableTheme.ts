92efd6820bbda0378c8ee7d63d02098a9f267f85
Lucas Silva <L.Silva.E@hotmail.com>
2025-05-04 20:52:59 -0400

    💄 Refactor: Improve table theme for better readability

--- src/themes/tableTheme.ts
+++ src/themes/tableTheme.ts
@@ -1,96 +1,37 @@
-// const TableTheme = {
-//   baseStyle: {
-//     table: {
-//       border: "2px solid",
-//       borderColor: "green.200",
-//       backgroundColor: "green.50",
-//       color: "green.700",
-//       _dark: {
-//         border: "1px solid",
-//         borderColor: "green.800",
-//         backgroundColor: "green.900",
-//         color: "green.200",
-//       },
-//     },
-//     thead: {
-//       th: {
-//         backgroundColor: "green.200",
-//         borderColor: "green.200",
-//         color: "green.900",
-//         _dark: {
-//           backgroundColor: "green.900",
-//           borderColor: "green.900",
-//           color: "green.100",
-//         },
-//       },
-//     },
-//     tbody: {
-//       td: {
-//         borderColor: "green.200",
-//         _dark: {
-//           borderColor: "green.800",
-//         },
-//       },
-//       tr: {
-//         _even: {
-//           backgroundColor: "green.100",
-//           _dark: {
-//             backgroundColor: "green.700",
-//           },
-//         },
-//         _odd: {
-//           backgroundColor: "green.50",
-//           _dark: {
-//             backgroundColor: "green.800",
-//           },
-//         },
-//       },
-//     },
-//   },
-// };
-
 const TableTheme = {
   baseStyle: {
     table: {
-      border: "2px solid",
+      border: "3px solid",
       borderColor: "green.200",
-      backgroundColor: "green.50",
-      color: "green.700",
+      backgroundColor: "green.200",
       _dark: {
-        border: "2px transparent",
-        borderColor: "gray.600",
-        backgroundColor: "red.900",
-        color: "white",
+        border: "3px solid",
+        borderColor: "green.800",
+        backgroundColor: "green.800",
       },
     },
     thead: {
       th: {
-        backgroundColor: "gray.200",
-        borderColor: "gray.200",
-        color: "gray.900",
+        backgroundColor: "green.200",
+        borderColor: "green.200",
+        color: "green.900",
         _dark: {
           backgroundColor: "green.800",
-          borderColor: "gray.800",
+          borderColor: "green.800",
           color: "green.100",
         },
       },
     },
     tbody: {
-      td: {
-        borderColor: "gray.200",
-        _dark: {
-          borderColor: "gray.800",
-        },
-      },
       tr: {
         _even: {
-          backgroundColor: "gray.100",
+          backgroundColor: "gray.50",
           _dark: {
             backgroundColor: "gray.600",
           },
         },
         _odd: {
-          backgroundColor: "gray.50",
+          backgroundColor: "gray.200",
           _dark: {
             backgroundColor: "gray.700",
           },
