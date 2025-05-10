6ced0c12b907fb540aff4221ba44027a992b97c2
Lucas Silva <L.Silva.E@hotmail.com>
2025-05-10 00:53:59 -0400

    ♻️  Feat: Display ingredient details directly from recipe datadient details.

--- src/pages/ViewRecipePage.tsx
+++ src/pages/ViewRecipePage.tsx
@@ -27,7 +27,7 @@ import useAxios from "../hooks/axiosFetch";
 import { API_BASE_URL } from "../constants/environment";
 import { HTTP_METHODS } from "../constants/httpMethods";
 import { useEffect, useState } from "react";
-import { IngredientDetail, Recipe } from "../types";
+import { Recipe } from "../types";
 import { useNavigate, useParams } from "react-router-dom";
 import {
   FaCheckSquare,
@@ -39,15 +39,12 @@ import {
 } from "react-icons/fa";
 import { setTimeText } from "../utils/utilities";
 
-const defaultIngredientState = { id: "0", quantity: "" };
-
 const ViewRecipePage = () => {
   const navigate = useNavigate();
   const { id } = useParams();
 
   const borderColor = useColorModeValue("gray.400", "gray.600");
 
-  const [ingredients, setIngredients] = useState([defaultIngredientState]);
   const [recipeData, setRecipeData] = useState({
     name: "",
     description: "",
@@ -60,8 +57,6 @@ const ViewRecipePage = () => {
     origin: "",
   });
 
-  const { data: dataIngredients, axiosFetch: axiosFetchIngredients } =
-    useAxios<IngredientDetail[]>();
   const { data: recipe, axiosFetch: axiosFetchRecipe } = useAxios<Recipe>();
 
   const [checkedIngredients, setCheckedIngredients] = useState<Set<string>>(
@@ -79,7 +74,6 @@ const ViewRecipePage = () => {
   };
 
   useEffect(() => {
-    axiosFetchIngredients(HTTP_METHODS.GET, `${API_BASE_URL}/ingredient`);
     axiosFetchRecipe(HTTP_METHODS.GET, `${API_BASE_URL}/recipe/?id=${id}`);
   }, []);
 
@@ -96,12 +90,6 @@ const ViewRecipePage = () => {
         category: recipe.category.name,
         origin: recipe.origin.name,
       });
-      setIngredients(
-        recipe.ingredients.map((ingredient) => ({
-          id: ingredient.ingredient.id.toString(),
-          quantity: ingredient.quantity.toString(),
-        }))
-      );
     }
   }, [recipe]);
 
@@ -180,11 +168,11 @@ const ViewRecipePage = () => {
                 </Tr>
               </Thead>
               <Tbody>
-                {ingredients.map((ingredient, index) => (
+                {recipe?.ingredients.map((ingredient, index) => (
                   <Tr
                     key={index}
                     color={
-                      checkedIngredients.has(ingredient.id)
+                      checkedIngredients.has(index.toString())
                         ? "green.500"
                         : "inherit"
                     }
@@ -193,26 +181,16 @@ const ViewRecipePage = () => {
                       <Checkbox
                         colorScheme="green"
                         borderColor={useColorModeValue("gray.400", "gray.500")}
-                        isChecked={checkedIngredients.has(ingredient.id)}
-                        onChange={() => toggleIngredient(ingredient.id)}
+                        isChecked={checkedIngredients.has(index.toString())}
+                        onChange={() => toggleIngredient(index.toString())}
                         size="lg"
                       />
                     </Td>
                     <Td textAlign="right">
                       {ingredient.quantity + " "}
-                      {
-                        dataIngredients?.find(
-                          (item) => item.id === parseInt(ingredient.id)
-                        )?.unit
-                      }
+                      {ingredient.ingredient.unit}
                     </Td>
-                    <Td>
-                      {
-                        dataIngredients?.find(
-                          (item) => item.id === parseInt(ingredient.id)
-                        )?.name
-                      }
-                    </Td>
+                    <Td>{ingredient.ingredient.name}</Td>
                   </Tr>
                 ))}
               </Tbody>
